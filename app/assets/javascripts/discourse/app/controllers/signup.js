a846e8bfa710e5aef4b2fd9197e46cfb2ff2f71a
Penar Musaraj <pmusaraj@gmail.com>
2025-03-12 16:03:08 -0400

    lint

--- app/assets/javascripts/discourse/app/controllers/signup.js
+++ app/assets/javascripts/discourse/app/controllers/signup.js
@@ -174,11 +174,10 @@ export default class SignupPageController extends Controller.extend(
     return this.passwordValidation.ok || this.passwordValidation.reason;
   }
 
-  @discourseComputed("usernameValidation.reason")
-  showUsernameInstructions(usernameValidationReason) {
+  get showUsernameInstructions() {
     return (
       this.siteSettings.show_signup_form_username_instructions &&
-      !usernameValidationReason
+      !this.usernameValidation.reason
     );
   }
 
